fed97046f91edf9ca454b86825e0440a231135d7
Swaff <swaff68@gmail.com>
2014-06-24 10:37:22 -0600

    functions2

--- main.js
+++ main.js
@@ -8,3 +8,44 @@ var square = function(x,y,z) {
 // or
 
 alert(square(3,4,4))
+
+var square = function(x){
+	return x * x;
+};
+
+var cube = function(x){
+	return x * square(x);
+};
+
+alert(cube(4));
+
+var input = "loren";
+
+var isEmpty = function(s){
+	return s.length === 0;
+};
+
+var inputIsEmpty = isEmpty(input);
+alert(inputIsEmpty)
+
+
+var pluralize = function(word,number){
+	if(number===1){
+		return word;
+	}
+	else {
+		return word + "s";
+	}
+};
+
+var isValidNumber = function(x){
+	var max = 100;
+	return x <=100;
+};
+
+var max = 75;
+var isValid = isValidNumber(80)
+;
+
+alert(max);
+alert(isValid);
